b7d667179710715ebd229596bb0540708f488a07
Vincentius <vincentius85@gmail.com>
2026-03-02 01:41:53 +0700

    feat: add the routes for the product

--- routes/routes.go
+++ routes/routes.go
@@ -13,4 +13,11 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/users/:id", handlers.GetUserByIdHandler)
 	r.PATCH("/users/:id", handlers.UpdateUserHandler)
 	r.DELETE("/users/:id", handlers.DeleteUserHandler)
+
+	//Product Routes
+	r.POST("/products", handlers.CreateProductHandler)
+	r.GET("/products", handlers.GetAllProductsHandler)
+	r.GET("/products/:id", handlers.GetProductByIdHandler)
+	r.PATCH("/products/:id", handlers.UpdateProductHandler)
+	r.DELETE("/products/:id", handlers.DeleteProductHandler)
 }
