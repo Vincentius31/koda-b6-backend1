013f61c1c66aa77915186b82f4152656b6633065
Vincentius <vincentius85@gmail.com>
2026-03-04 16:10:51 +0700

    feat: adding routes for swaygo docs

--- routes/routes.go
+++ routes/routes.go
@@ -2,12 +2,16 @@ package routes
 
 import (
 	"koda-b6-backend1/handlers"
+
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	docs "koda-b6-backend1/docs"
 )
 
 func SetupRoutes(r *gin.Engine) {
 	// Routes
-	r.POST("/register", handlers.RegisterHandler)
+	r.POST("/users", handlers.CreateHandler)
 	r.POST("/login", handlers.LoginHandler)
 	r.GET("/users", handlers.GetAllUsersHandler)
 	r.GET("/users/:id", handlers.GetUserByIdHandler)
@@ -20,4 +24,11 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/products/:id", handlers.GetProductByIdHandler)
 	r.PATCH("/products/:id", handlers.UpdateProductHandler)
 	r.DELETE("/products/:id", handlers.DeleteProductHandler)
+
+	//Docs
+	docs.SwaggerInfo.BasePath = "/" 
+	docsPath := r.Group("/docs")
+	{
+		docsPath.GET("/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 }
